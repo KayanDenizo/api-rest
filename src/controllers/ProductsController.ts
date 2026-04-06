05c167d7ebf2e5d8af3e5dd94010bb5d329e5c26
kayandenizo <kayan.denizo@gmail.com>
2026-04-06 17:33:46 -0300

    tratamento de exceções utilizando o Express

--- src/controllers/ProductsController.ts
+++ src/controllers/ProductsController.ts
@@ -17,6 +17,8 @@ class ProductsController {
     create(request: Request, response: Response) {
         const { name, price } = request.body
 
+        throw new Error("Errro de exemplo")
+
         response.status(201).json({ name, price, user_id: request.user_id })
     }
 
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 // Subir servidor, criar GET /products, POST e usar query e body
-import express, { response } from "express";
+import express, {Request, Response, NextFunction } from "express";
 import { routes } from "./routes";
 
 const PORT = 3333; // Boa pratica pois se futuramente quiser mudar a porta fica mais facil
 
 const app = express(); // Inicializa o servidor Express
 
-app.use(express.json()) 
+app.use(express.json())
 
 app.use(routes)
+
+app.use((error: any, request: Request, response: Response, _: NextFunction) => {
+    response.status(500).json({ message: "Erro no servidor" })
+
+})
 
 app.listen(PORT, () => console.log(`Server is running at ${PORT}`))
