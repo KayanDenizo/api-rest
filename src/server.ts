d19671b9226ca2548b1113632834e06e6e14f807
kayandenizo <kayan.denizo@gmail.com>
2026-04-15 18:20:31 -0300

    Formatando mensagens de erro

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import { routes } from "./routes";
 import { AppError } from "./utils/AppError";
+import { ZodError } from "zod";
 
 const PORT = 3333; // Boa pratica pois se futuramente quiser mudar a porta fica mais facil
 
@@ -13,8 +14,17 @@ app.use(routes)
 
 app.use((error: any, request: Request, response: Response, _: NextFunction) => {
     if (error instanceof AppError) {
-        return response.status(error.statusCode).json({ message: error.message })
+        return response
+            .status(error.statusCode)
+            .json({ message: error.message })
     }
+
+    if (error instanceof ZodError) {
+        return response
+            .status(400)
+            .json({ message: "Validation Error!", issues: error.format() }) //Requisição ruim, criterios nao atendidos
+    }
+
     response.status(500).json({ message: "Erro no servidor" })
 
 })
